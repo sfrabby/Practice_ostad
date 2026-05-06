b0362fb6f9bd501505175a0e5e8d78747747fc90
sfrabby <fazlerabby6732@gmail.com>
2026-05-06 23:42:19 +0600

    Appbar done

---
+++ lib/MyHomePage.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+        title: Text("Ostad App"),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.add)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.edit)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.person)),
+        ],
+      ),
+    );
+  }
+}
